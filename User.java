ce83c0dba416d4419797b9017c922b7b3dc393cb
Andrew <andrewpstrickland@gmail.com>
2017-03-07 10:32:37 -0500

    t1

---
+++ User.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Andrew on 3/6/2017.
+ */
+public class User{
+    private static String name;
+    private static String id;
+    public void info(String name, String id){
+        name = User.name;
+        id = User.id;
+
+    }
+}

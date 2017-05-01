4e3e9e2699d6f932542a3c5c9e40d97529682b08
Andrew <andrewpstrickland@gmail.com>
2017-05-01 00:05:18 -0400

    version1.25: windows size sets to screen-100, change user id button added.

---
+++ TimeClockTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by Andrew on 5/1/2017.
+ */
+public class TimeClockTest {
+    @org.junit.Test
+    public void setRecordText() throws Exception {
+        throw new IllegalArgumentException();
+    }
+
+}
